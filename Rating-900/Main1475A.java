27c3b8966e1f8f5c7605f583de8a3d7f791c30fe
Awaneesh0 <awaneesh209@gmail.com>
2026-08-05 22:43:17 +0530

    Add files via upload

---
+++ Rating-900/Main1475A.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main1475A {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt(); 
+        while (t-- > 0) {
+            long n = sc.nextLong(); 
+            if ((n & (n - 1)) == 0) {
+                System.out.println("NO");
+            } else {
+                System.out.println("YES");
+            }
+        }
+        
+        sc.close();
+    }
+}
