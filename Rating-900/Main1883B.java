a2fc55b0747ea509f901d96ad4d436d456f51bb1
Awaneesh0 <awaneesh209@gmail.com>
2026-08-10 19:54:22 +0530

    Add files via upload

---
+++ Rating-900/Main1883B.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main1883B {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            String s = sc.next();
+            
+            int[] freq = new int[26];
+            
+            for (int i = 0; i < n; i++) {
+                freq[s.charAt(i) - 'a']++;
+            }
+            
+            int oddCount = 0;
+            
+            for (int i = 0; i < 26; i++) {
+                if (freq[i] % 2 != 0) {
+                    oddCount++;
+                }
+            }
+            
+            if (k >= oddCount - 1) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+        
+        sc.close();
+    }
+}
